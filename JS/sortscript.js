384f3e4209d5c0fbad84736b6a9aed21e3e090b5
John Armstrong <johnarmstrong117@gmail.com>
2025-04-20 23:31:07 -0700

    Adding more algorithms to sorting options

--- JS/sortscript.js
+++ JS/sortscript.js
@@ -25,6 +25,8 @@ async function startSort(){
     const algo = document.getElementById("algorithm").value;
     if (algo === "bubble") await bubbleSort();
     else if (algo === "selection") await selectionSort();
+    else if (algo === "insertion") await insertionSort();
+    else if (algo === "merge") await mergeSort(array, 0, array.length -1);
 }
 
 async function bubbleSort(){
@@ -77,6 +79,76 @@ async function selectionSort(){
         playNote(array[i]);
         await sleep(30);
     }
+}
+
+async function insertionSort(){
+    const bars = document.getElementById("bar");
+    for (let i = 1; i < array.length; i++){
+        let key = array[i];
+        let j = i - 1;
+
+        while (j >= 0 && array[j] > key){
+            array[j + 1] = array[j];
+            bars[j + 1].style.height = `${array[j + 1]}px`
+            playNote(array[j + 1]);
+            await sleep(30);
+            j--;
+        }
+
+        array[j + 1] = key;
+        bars[j + 1].style.height = `${key}px`;
+        playNote(key);
+        await sleep(30);
+    }
+}
+
+async function merge(start, mid, end){
+    const left = array.slice(start, mid + 1);
+    const right = array.slice(mid + 1, end + 1);
+
+    let i = 0, j = 0, k = start;
+    const bars = document.getElementsByClassName("bar");
+
+    while (i < left.length && j < right.length) {
+        if (left[i] <= right[j]) {
+            array[k] = left[i];
+            bars[k].style.height = `${left[i]}px`;
+            playNote(left[i]);
+            i++;
+        } else {
+            array[k] = right[j];
+            bars[k].style.height = `${right[j]}px`;
+            playNote(right[j]);
+            j++;
+        }
+        await sleep(30);
+        k++;
+    }
+
+    while (i < left.length) {
+        array[k] = left[i];
+        bars[k].style.height = `${left[i]}px`;
+        playNote(left[i]);
+        await sleep(30);
+        i++; k++;
+    }
+
+    while (j < right.length) {
+        array[k] = right[j];
+        bars[k].style.height = `${right[j]}px`;
+        playNote(right[j]);
+        await sleep(30);
+        j++; k++;
+    }
+}
+
+async function mergeSort(arr, start, end){
+    if (start >= end) return;
+
+    const mid = Math.floor((start + end) / 2);
+    await mergeSort(arr, start, mid);
+    await mergeSort(arr, mid + 1, end);
+    await merge(start, mid, end);
 }
 
 function sleep(ms){
