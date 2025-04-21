ca5603cdd5cf54cb79b0a8fab59300bd8c2ade4c
John Armstrong <johnarmstrong117@gmail.com>
2025-04-20 23:35:49 -0700

    Fixing insertion sort

--- JS/sortscript.js
+++ JS/sortscript.js
@@ -82,14 +82,14 @@ async function selectionSort(){
 }
 
 async function insertionSort(){
-    const bars = document.getElementById("bar");
+    const bars = document.getElementsByClassName("bar");
     for (let i = 1; i < array.length; i++){
         let key = array[i];
         let j = i - 1;
 
         while (j >= 0 && array[j] > key){
             array[j + 1] = array[j];
-            bars[j + 1].style.height = `${array[j + 1]}px`
+            bars[j + 1].style.height = `${array[j + 1]}px`;
             playNote(array[j + 1]);
             await sleep(30);
             j--;
